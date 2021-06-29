5bd1eb242ba9ba3c348550b76da8e962a8e073c3
tjswlsaa <85541352+tjswlsaa@users.noreply.github.com>
2021-06-29 13:28:46 +0900

    fourth commit

--- pages/login/emailSignup.js
+++ pages/login/emailSignup.js
@@ -39,7 +39,7 @@ const firebaseConfig = {
 
    
 
-// 비밀번호랑 비밀번호 확인이 일치하면 형식까지 맞는지 확인하고 true 리턴하는 함수 
+// 비밀번호랑 비밀번호 확인이 일치하면, 형식까지 맞는지 확인하고 true 리턴하는 함수 
 checkPassword= (password,confirmPassword)=> {
     if (password !== confirmPassword) {
         alert("비밀번호가 일치하지 않습니다!");}
@@ -47,7 +47,7 @@ checkPassword= (password,confirmPassword)=> {
     else{
       //  isPassword(password) {
             var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/; //  8 ~ 10자 영문, 숫자 조합
-            return regExp.test(password); // 형식에 맞는 경우 true 리턴}
+            return regExp.test(password); // 형식에 맞는 경우 true 리턴} // 이함수도 제대로 안되고 있는듯 
                }
 }
 
@@ -67,54 +67,116 @@ checkPassword= (password,confirmPassword)=> {
 //     }
 //   }
 // [Error: The email address is already in use by another account.] 이미 알아서 alert가 뜨고 있는데 이걸 유져한테 보일 방법이 있을까??
+//[Error: The email address is already in use by another account.] 새로운 이메일인 데도 이런 오류가 뜨곤함 
 
-checkEmail=(email)=>{
+      checkEmail=(email)=>{
+        console.log('checkEmail()');
+        console.log({email});
         var regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-        return regExp.test(email); // 형식에 맞는 경우 true 리턴
-      //여기에 중복확인 함수 넣고 싶다....
-}
+        const isCheckEmail = regExp.test(email);
+        console.log({isCheckEmail});
+        return isCheckEmail;
+        // return regExp.test(email); // 형식에 맞는 경우 true 리턴
+        //여기에 중복확인 함수 넣고 싶다....
+      }
 
       handleSignUp = async(email,password,confirmPassword) => {
+        console.log('handleSignUp()');
+        console.log({email,password,confirmPassword});
      
-      try {
-    
-        await this.checkEmail()
-        await this.checkPassword()
+      // try {
+        const isCheckEmail = await this.checkEmail(email); // 함수와 함수 파라미터의 관계
+        console.log({isCheckEmail});
+        if (isCheckEmail == false) {
+          return; // early return
+        }
 
+        const isCheckPassword = await this.checkPassword(password,confirmPassword);
+        console.log({isCheckPassword});
+        if (isCheckPassword == false) {
+          return '-_-?'; // early return
+        }
+        
         // alert나고도 바로 회원가입이 되는 문제... 이게 둘다 true로 리턴되면 아래 함수를 시행하라는 코드는 어떻게 짜나요.?
 
-        firebase.auth()
+        console.log('.');
+        const userCredential = await firebase.auth()
             .createUserWithEmailAndPassword(email, password)
-            .catch(error => console.log(error))
-        const credential = firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(error => {
+              console.log({error});
+              alert(error);
+            });
+        console.log('..');
+        // 좌항과 우항
+        // const userCredential = firebase.auth().createUserWithEmailAndPassword(email, password);
+        console.log({userCredential});
+        const isValid = (userCredential > ''); // 제가 (이은국씨가) 개인적으로 즐겨하는 truthy 체크 방법
+        if (isValid == false) {
+          return; // 이 아래로 진행시키지 않는다.
+        }
+
+        console.log({'userCredential.user': userCredential.user});
+
+        firebase
+          .database()
+          .ref('/users/' + userCredential.user.uid)
+          .set({
+            email: userCredential.user.email,
+            user_uid: userCredential.user.uid,
+            created_at: Date.now(),
+          })
+          .then(function(snapshot) {
+            console.log('Snapshot', snapshot);
+          });
+        
 
         // 회원가입 후 발급받은 credential로 바로 로그인하고 database에 저장까지
         // Error: signInWithCredential failed: First argument "credential" must be a valid credential.이렇게뜸 
         // 그럼에도 바로 로그인 되는거보니 필요없는 함수인가...? 
-        //저장은 한번도 성공 못함...
-        firebase.auth()
-                .signInWithCredential(credential)
-                .then(function(result) {
-                  console.log('user signed in ');
-                    firebase
-                      .database()
-                      .ref('/users/' + result.user.uid)
-                      .set({
-                        email: result.user.email,
-                        user_uid:result.user.uid,
-                        created_at: Date.now()
-                      })
-                      .then(function(snapshot) {
-                        console.log('Snapshot', snapshot);
-                      });
+        //database 저장은 한번도 성공 못함...
+        // firebase.auth()
+        //         .signInWithCredential(credential) 
+        //         // signInWithCredential은 뭣이고, credential은 뭣인가!
+        //         // signIn 즉 로그인의, With 즉 방식은
+        //         // 다양한 방법이 있다.
+        //         // 그 중에서 Credential은.. Credential로 로그인 하는 것이고,
+        //         // 그 밖에 .signInWithEmailAndPassword() 등등이 있다.
+        //         // 즉, Credential은 다른 로그인 주체가, 해당 로그인 주체의 아이디/패스워드로
+        //         // 로그인이 정상적으로 되었음을 증명하는 방식이다! (사실 겁나 생략되어있음)
+
+        //         .then(function(result) {
+        //           // then: signInWithCredential() 이 성공하면, 이 부분을=이 함수를 실행합니다!
+        //           // * 근데 왜, 굳이? 함수로 어쩌구 하나요? *
+        //           // -> callback을 알아야 하고, 그래서 async-await를 알아야 하고, 그래서 promise를 알아야 하고!
+        //           // -->> 그래서 다시, callback을 대체 왜 쓰는지? 알아야 하고
+        //           // --->>> 그래서, 또다시, 비동기 프로그래밍이 뭔지? 왜 쓰는지? 왜 일케 귀찮게 하는지!
+        //           // ....를 알아야 한다!
+
+        //           console.log('firebase.auth().signInWithCredential(credential)');
+        //           // ` : javascript template literal
+        //           // 제가 멋부릴려고 쓴거에 더 가깝습니다 T_T;
+        //           console.log({credential, result});
+        //           console.log('user signed in ');
+        //             firebase
+        //               .database()
+        //               .ref('/users/' + result.user.uid)
+        //               .set({
+        //                 email: result.user.email,
+        //                 user_uid:result.user.uid,
+        //                 created_at: Date.now()
+        //               })
+        //               .then(function(snapshot) {
+        //                 console.log('Snapshot', snapshot);
+        //               });
                  
 
-        })}
+        // })}
         
-        catch (error) {
-            console.log(error.toString(error));
-          }
-        }
+        // catch (error) {
+        //     console.log(error.toString(error));
+        //   }
+        // }
+      }
 
     render () {
       return(
